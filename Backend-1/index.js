139931def8b4de6fd26ddf4924e4afdee3a3efc8
Adityakanojia24 <adityakanojia63830@gmail.com>
2022-11-12 11:57:08 +0530

    update

--- Backend-1/index.js
+++ Backend-1/index.js
@@ -50,14 +50,14 @@ app.delete("/tasks/:id",async (req,res)=>{
     }
 })
 
-if(process.env.NODE_ENV==='production'){
+// if(process.env.NODE_ENV==='production'){
   const path = require('path')
 
   app.get('/',(req,res)=>{
       app.use(express.static(path.resolve(__dirname,'client','build')))
       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
   })
-}
+// }
 
 databaseconnection();
 
